9b87ed6e675a086a724be7e9edf6f369e9f3439b
YashHasija <hasijayashwork@gmail.com>
2025-08-07 11:44:41 +0530

    final code

--- self_intro_script_generator_project1.py
+++ self_intro_script_generator_project1.py
@@ -1,19 +1,22 @@
 # creating a self intro script generator project 1 
 
+# importing date and time library 
+import datetime 
 # .strip()- This ensures that if the user accidentally adds spaces before or after their name, those spaces are removed.
-import datetime
 name = input("what is your name").strip()
 age = input("How old are you").strip()
 city = input("Which city do you live?").strip()
 proffesion =input("what is your proffesion").strip()
 hobby =input("what is your hoby?").strip()
 
+# f is formatted string - It allows you to directly insert the values of variables inside curly braces {} within the string.
 intro_message = (
-    f"Hello! My name is {name}, I'm {age} years old and live in {city}.\n"
+    f"Hello! My name is {name}, I'm {age} years old and live in {city}.\n"   # \n is used for line break 
     f"I work as a {proffesion} and I absolutely enjoy {hobby} in my free time.\n"
     "Nice to meet you!\n"
 )
 
+# getting today date using datetime module / coverting date to a string using iso 
 current_date = datetime.date.today().isoformat()
 intro_message += f"\n Logged on : {current_date}"
 
@@ -22,8 +25,4 @@ final_output = f"{border}\n{intro_message}\n{border}"
 
 print("\n" + final_output)
 
-#intro_message = (
-   # f"Hello! My name is {name} , I'm {age} years old and  live in {city}"
-   # f"I work as a {proffesion} and i absolutely enjoy {hobby} in my free time"
-   # f"Nice to meet you!\n" # \n is used for new line 
-   # )
+
