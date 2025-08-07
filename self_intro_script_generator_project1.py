604fe94a8854d4ad1df0ee6819b5817050f3bd4c
YashHasija <hasijayashwork@gmail.com>
2025-08-07 11:32:25 +0530

    Modified self intro script generator project 1

--- self_intro_script_generator_project1.py
+++ self_intro_script_generator_project1.py
@@ -5,16 +5,14 @@ import datetime
 name = input("what is your name").strip()
 age = input("How old are you").strip()
 city = input("Which city do you live?").strip()
-proffesion = ("what is your proffesion").strip()
-hobby = ("what is your hoby?").strip()
+proffesion =input("what is your proffesion").strip()
+hobby =input("what is your hoby?").strip()
 
 intro_message = (
-    f"Hello! My name is {name} , I'm {age} years old and
-    live in {city}"
-    f"I work as a {proffesion} and i absolutely enjoy {hobby} 
-    in my free time"
-    f"Nice to meet you!\n" # \n is used for new line 
-    )
+    f"Hello! My name is {name}, I'm {age} years old and live in {city}.\n"
+    f"I work as a {proffesion} and I absolutely enjoy {hobby} in my free time.\n"
+    "Nice to meet you!\n"
+)
 
 current_date = datetime.date.today().isoformat()
 intro_message += f"\n Logged on : {current_date}"
@@ -23,3 +21,9 @@ border = "*" * 80
 final_output = f"{border}\n{intro_message}\n{border}"
 
 print("\n" + final_output)
+
+#intro_message = (
+   # f"Hello! My name is {name} , I'm {age} years old and  live in {city}"
+   # f"I work as a {proffesion} and i absolutely enjoy {hobby} in my free time"
+   # f"Nice to meet you!\n" # \n is used for new line 
+   # )
